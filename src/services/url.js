d1dd9d49a85a85d9e5d167ef2f7a9c0c38f05921
unknown <missionhach@gmail.com>
2025-08-08 19:46:02 +0530

    modified for the prod

--- src/services/url.js
+++ src/services/url.js
@@ -1,2 +1,3 @@
-export const EmployeeDirectoryUrl = "/api/v1/Employees";
-export const LeaveRequestsUrl = "/api/v1/Leave-request";
+export const API_BASE = import.meta.env.VITE_API_BASE;
+export const EmployeeDirectoryUrl = `${API_BASE}/v1/Employees`;
+export const LeaveRequestsUrl = `${API_BASE}/v1/Leave-request`;
